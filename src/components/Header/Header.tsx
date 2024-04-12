7fa9a192cba8699c28e659ffd56951ea687a9771
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-04-12 20:08:13 -0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import style from './Header.module.css'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className={style.header}>
+      <strong>todo</strong>
+    </header>
   )
 }
