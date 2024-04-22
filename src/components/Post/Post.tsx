24312346fe35039b50465ffe5b8b2059b2eb8303
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-04-22 09:13:58 -0300

    change text language

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -3,19 +3,19 @@ import style from "./Post.module.css";
 export default function Post() {
   return (
     <article className={style.todo}>
-      
+
       <form className={style.formComment}>
-        <textarea name="comment" placeholder="Adicione uma nova tarefa" />
-        <button>Criar</button>
+        <textarea name="comment" placeholder="Add a new task" />
+        <button>Create</button>
       </form>
 
       <div>
-        <p>Tarefas criadas: 0</p>
-        <p>Concluídas: 0</p>
+        <span>Created tasks: 0</span>
+        <span>Completed: 0</span>
       </div>
 
       <div>
-        <h2>Você ainda não tem tarefas cadastradas</h2>
+        <h2>You don't have any tasks registered yet</h2>
       </div>
     </article>
   );
