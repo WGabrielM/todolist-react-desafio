ea0ca3c8b41671c8c8558fa0512ee82f451cfabd
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-04-22 08:49:38 -0300

    style textarea and button post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,16 +1,22 @@
-import style from './Post.module.css'
+import style from "./Post.module.css";
 
 export default function Post() {
   return (
-    <article>
-        <form>
-            <textarea></textarea>
-            <button>Criar</button>
-        </form>
+    <article className={style.todo}>
+      
+      <form className={style.formComment}>
+        <textarea name="comment" placeholder="Adicione uma nova tarefa" />
+        <button>Criar</button>
+      </form>
 
-        <footer>
-            
-        </footer>
+      <div>
+        <p>Tarefas criadas: 0</p>
+        <p>Concluídas: 0</p>
+      </div>
+
+      <div>
+        <h2>Você ainda não tem tarefas cadastradas</h2>
+      </div>
     </article>
-  )
+  );
 }
