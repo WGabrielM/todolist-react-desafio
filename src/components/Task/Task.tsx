d4a67e78a3d2fe2b14e345d6297495167f32174c
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-04-26 20:21:14 -0300

    add post task

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import style from "./Task.module.css";
 
 export default function Task() {
+  const [have, setHave] = useState(true);
   return (
-    <div className={style.task}>
-      <div className={style.notRegistered}>
-        <img src="/src/assets/clipboard.svg" alt="" />
-        <span>You don't have any tasks registered yet</span>
-        <p>Crie tarefas e organize seus itens a fazer</p>
-      </div>
-    </div>
+    <>
+      {have ? (
+       <Post />
+      ) : (
+        <div className={style.task}>
+          <div className={style.notRegistered}>
+            <img src="/src/assets/clipboard.svg" alt="" />
+            <span>You don't have any tasks registered yet</span>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
